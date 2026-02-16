6ba1a82de3e8fbbf8475ddcd4cbfedf2c3a949e0
Koni <konstantin.zeck+github@gmail.com>
2026-02-16 02:05:28 -0800

    Adding lightning module for keypoint detection task training pipeline

---
+++ src/keypoint_task.py
@@ -0,0 +1,45 @@
+import lightning as L
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from model import Simple_CNN
+
+class KeypointDetection(L.LightningModule):
+
+    def __init__(self, model, criterion, lr=4e-3):
+        super().__init__()
+        self.model = torch.compile(model)
+        self.save_hyperparameters(ignore=["out_dim", "model"])
+    
+    def training_step(self, batch):
+        inputs, targets = batch['image'], batch['keypoints']
+        outputs = self.model(inputs)
+        loss = self.hparams.criterion(outputs, targets.view(targets.size(0), -1))
+        self.log("train_loss", loss, prog_bar=True, on_epoch=True, logger=True)
+        return loss
+    
+    def validation_step(self, batch):
+        inputs, targets = batch['image'], batch['keypoints']
+        outputs = self.model(inputs)
+        loss = self.hparams.criterion(outputs, targets.view(targets.size(0), -1))
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True, logger=True)
+        return loss
+    
+    def test_step(self, batch):
+        inputs, targets = batch['image'], batch['keypoints']
+        outputs = self.model(inputs)
+        loss = self.hparams.criterion(outputs, targets.view(targets.size(0), -1))
+        self.log("test_loss", loss, logger=True)
+        return loss
+    
+    def predict_step(self, input):
+        return self.model(input)
+    
+    def forward(self, input):
+        self.model.eval()
+        with torch.no_grad():
+            return self.model(input)
+        
+    def configure_optimizers(self):
+        return optim.Adam(self.model.parameters(), lr=self.hparams.lr)
