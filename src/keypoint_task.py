369333b7b881bd92943d260c5c41665f34b22f88
Koni <konstantin.zeck+github@gmail.com>
2026-02-22 20:24:49 -0800

    Change shared step function, fix hyperparameter treatment

--- src/keypoint_task.py
+++ src/keypoint_task.py
@@ -1,42 +1,48 @@
 import lightning as L
 import torch
+import torch.nn as nn
 import torch.optim as optim
 
 
 class KeypointDetection(L.LightningModule):
-    def __init__(self, model, criterion, lr=4e-3):
+    def __init__(self, model: nn.Module, lr: float = 4e-3, criterion: str = "mse"):
         super().__init__()
-        self.model = torch.compile(model)
-        self.save_hyperparameters(ignore=["out_dim", "model"])
 
-    def training_step(self, batch):
-        inputs, targets = batch["image"], batch["keypoints"]
-        outputs = self.model(inputs)
-        loss = self.hparams.criterion(outputs, targets.view(targets.size(0), -1))
-        self.log("train_loss", loss, prog_bar=True, on_epoch=True, logger=True)
-        return loss
+        self.save_hyperparameters(ignore=["model"])
 
-    def validation_step(self, batch):
-        inputs, targets = batch["image"], batch["keypoints"]
-        outputs = self.model(inputs)
-        loss = self.hparams.criterion(outputs, targets.view(targets.size(0), -1))
-        self.log("val_loss", loss, prog_bar=True, on_epoch=True, logger=True)
-        return loss
-
-    def test_step(self, batch):
-        inputs, targets = batch["image"], batch["keypoints"]
-        outputs = self.model(inputs)
-        loss = self.hparams.criterion(outputs, targets.view(targets.size(0), -1))
-        self.log("test_loss", loss, logger=True)
-        return loss
-
-    def predict_step(self, input):
-        return self.model(input)
+        self.model = model
+        self.criterion = nn.MSELoss() if criterion == "mse" else nn.SmoothL1Loss()
 
     def forward(self, input):
         self.model.eval()
         with torch.no_grad():
-            return self.model(input)
+            return self(input)
+
+    def _shared_step(self, batch, stage: str):
+        inputs, targets = batch["image"], batch["keypoints"]
+        outputs = self(inputs)
+        loss = self.criterion(outputs, targets.view(targets.size(0), -1))
+        self.log(
+            f"{stage}_loss",
+            loss,
+            prog_bar=True,
+            on_step=(stage == "train"),
+            on_epoch=True,
+            logger=True,
+        )
+        return loss
+
+    def training_step(self, batch):
+        return self._shared_step(batch, "train")
+
+    def validation_step(self, batch):
+        return self._shared_step(batch, "val")
+
+    def test_step(self, batch):
+        return self._shared_step(batch, "test")
+
+    def predict_step(self, input):
+        return self(input)
 
     def configure_optimizers(self):
-        return optim.Adam(self.model.parameters(), lr=self.hparams.lr)
+        return optim.Adam(self.parameters(), lr=self.hparams.lr)
