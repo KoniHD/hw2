a2470a0ffd935bc9897dd4a89c26b4d90c54110a
Koni <konstantin.zeck+github@gmail.com>
2026-02-22 20:23:14 -0800

    adding elu activation option

--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ from huggingface_hub import PyTorchModelHubMixin
 
 ACTIVATION_MAP = {
     "relu": nn.ReLU,
+    "elu": nn.ELU,
     "leaky_relu": nn.LeakyReLU,
     "tanh": nn.Tanh,
     "sigmoid": nn.Sigmoid,
@@ -40,29 +41,34 @@ class Simple_CNN(
         act = ACTIVATION_MAP[activation]
 
         self.net = nn.Sequential(
+            # Input: [B, 1, 224, 224]
             # Block 1
-            nn.Conv2d(1, 8, kernel_size=5, padding=1),  # 4*4*1*8 + 8 = 136
+            nn.Conv2d(1, 8, kernel_size=5, padding=1),  # [B, 8, 222, 222]
             nn.BatchNorm2d(8),
             act(),
-            nn.MaxPool2d(2, stride=2),
+            nn.Dropout(p=dropout),
+            nn.MaxPool2d(2, stride=2),  # [B, 8, 111, 111]
             # Block 2
-            nn.Conv2d(8, 16, kernel_size=4, padding=1),  # 3*3*8*16 + 16 = 1168
+            nn.Conv2d(8, 16, kernel_size=4, padding=1),  # [B, 16, 110, 110]
             nn.BatchNorm2d(16),
             act(),
-            nn.MaxPool2d(2, stride=2),
+            nn.Dropout(p=dropout),
+            nn.MaxPool2d(2, stride=2),  # [B, 16, 55, 55]
             # Block 3
-            nn.Conv2d(16, 16, kernel_size=4, padding=1),  # 3*3*16*16 + 16 = 2320
+            nn.Conv2d(16, 16, kernel_size=4, padding=1),  # [B, 16, 54, 54]
             nn.BatchNorm2d(16),
             act(),
-            nn.MaxPool2d(3, stride=3),
-            nn.Flatten(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool2d(3, stride=3),  # [B, 16, 18, 18]
+            nn.Flatten(),  # [B, 16 * 18 * 18] = [B, 5184]
             # FC Layers with Dropout
-            nn.Linear(16 * 18 * 18, 1024),  # 16*18*18*1024 = 5_308_416
+            nn.Linear(16 * 18 * 18, 1024),  # [B, 1024]
             nn.BatchNorm1d(1024),
             act(),
             nn.Dropout(p=dropout),
-            nn.Linear(1024, out_dim),  # 1024 * 136 = 139_264
+            nn.Linear(1024, out_dim),  # [B, 136]
             nn.Tanh(),
+            # Output: [B, 136] -> reshaped to [B, 68, 2] by caller if needed
         )
 
     def forward(self, input):
